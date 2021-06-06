61ad2d4c8981c121ed9aa39b67c340be976ec8c7
Harv38 <tush38635@gmail.com>
2021-06-06 13:09:07 +0530

    this

--- React_ES6/Spread/Nested.js
+++ React_ES6/Spread/Nested.js
@@ -9,12 +9,15 @@ let state = {
     }
 }
 
+// let copy = {...state}
+//This is known as our shallow copy
+
+// Shallow copy int his context means that for any given object that is spread, the uppermost level
+//  of the new variable is an object containing the same property values of the original object, but at a new
+//  refrence in the memory.
+// Any lower level nested objects however, will keep on  ointing to their original refrences
+
 // let copy = {...state,address:{...state.address}};
-
-
-
-
-
 // let copy= {...state,address:{...state.address,country:{...state.address.country}}}
 
 let copy = JSON.parse(JSON.stringify(state));
