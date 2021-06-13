1b9488e93cbfd0f64873d3514bb3993b7ad306ef
Harv38 <tush38635@gmail.com>
2021-06-13 13:35:58 +0530

    Search

--- React/movies/src/Components/Movies.js
+++ React/movies/src/Components/Movies.js
@@ -6,8 +6,7 @@ export default class Movies extends Component {
         super(props);
         this.state={
             movies:getMovies(),
-            currSearchText:'',
-            filterMovies:getMovies()
+            currSearchText:''
         }
     }
 
@@ -22,9 +21,46 @@ export default class Movies extends Component {
     }
     handleChange=(e)=>{
         let val = e.target.value;
-      
+        this.setState({
+            currSearchText:val
+        })
+        //We are kind of creating two states for similar content. As the filter movies operation is temporary and occurs with the state change
+        //  of currSearchText we can simply form the filterMovies array in the render method itself. So there is no need to make
+        // it as a state.
+        // if(val=='')
+        // {
+        //     this.setState({
+        //     filterMovies:this.state.movies,
+        //     currSearchText:''
+        // })
+        // return;
+        // }
+        // let filteredArr = this.state.movies.filter(movieObj=>{
+        //     let title = movieObj.title.trim().toLowerCase();
+        //     // console.log(title);
+        //     return title.includes(val.toLowerCase());
+        // })
+        // this.setState({
+        //     filterMovies:filteredArr,
+        //     currSearchText:val
+        // })
     }
     render() {
+        console.log('render');
+        let {movies,currSearchText}=this.state;
+        let filterMovies =[];
+        if(currSearchText!='')
+        {
+            filterMovies = movies.filter(movieObj=>{
+                    let title = movieObj.title.trim().toLowerCase();
+                    // console.log(title);
+                    return title.includes(currSearchText.toLowerCase());
+                })
+        }
+        else
+        {
+            filterMovies=movies;
+        }
         return (
             <div className='container'>
             <div className='row'>
@@ -33,7 +69,7 @@ export default class Movies extends Component {
                 </div>
                 <div className='col-9'>
                    <input onChange={this.handleChange} type='text'></input>
-                   <table class="table">
+                   <table className="table">
   <thead>
     <tr>
       
@@ -46,13 +82,13 @@ export default class Movies extends Component {
   </thead>
   <tbody>
     {
-        this.state.movies.map(movieObj=>(
-            <tr scope='row'>
+        filterMovies.map(movieObj=>(
+            <tr scope='row' key={movieObj._id} >
               <td>{movieObj.title}</td>
               <td>{movieObj.genre.name}</td>
               <td>{movieObj.numberInStock}</td>
               <td>{movieObj.dailyRentalRate}</td>
-              <td><button onClick={()=>this.onDelete(movieObj._id)} type="button" class="btn btn-danger">Delete</button></td>  
+              <td><button onClick={()=>this.onDelete(movieObj._id)} type="button" className="btn btn-danger">Delete</button></td>  
             </tr>
         ))
     }
